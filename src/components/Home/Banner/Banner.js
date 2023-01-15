64ec533c42ad058c7384398427c3cfd76654a431
nakibuddin <nakibaust137@gmail.com>
2023-01-15 17:24:43 +0600

    aos animation package added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -18,7 +18,9 @@ const Banner = () => {
                     {/* <img className='rounded-md' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxIMpcng0PpqR2S4Ct0htq3Cn7NCrdr4z9Tg&usqp=CAU" alt="" /> */}
                     <img className='rounded-md' src={Photo} alt="" />
                 </div>
-                <div className="flex flex-col w-full p-6 lg:w-2/3 md:p-8 lg:p-12">
+                <div data-aos="fade-left"
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-duration="2000" className="flex flex-col w-full p-6 lg:w-2/3 md:p-8 lg:p-12">
                     {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 mb-8  text-violet-400">
                         <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                     </svg> */}
